ea24e070fca5d682d5ddae68a33329e0da550b57
Lisa Vitolo <syn.shainer@gmail.com>
2013-06-19 21:39:59 +0200

    Restyling of feature collection methods

--- FeatureCollector.java
+++ FeatureCollector.java
@@ -15,6 +15,9 @@ import java.io.IOException;
 import edu.cmu.lti.ws4j.WS4J;
 import edu.cmu.lti.ws4j.util.*;
 import edu.cmu.lti.ws4j.impl.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Iterator;
 
 /**
  * @author shainer
@@ -22,29 +25,34 @@ import edu.cmu.lti.ws4j.impl.*;
 public class FeatureCollector
 {
     private SentencePair sp;
+    HashMap<String, Double> featureMap;
     
     public FeatureCollector()
-    {}
+    {
+        featureMap = new HashMap<String, Double>();
+    }
+    
+    private void printSentence(List<TaggedToken> s)
+    {
+        for (TaggedToken tt : s) {
+            System.out.print(tt.token + "_" + tt.tag + " ");
+        }
+        System.out.println();
+    }
     
     public HashMap<String, Double> features(SentencePair sp)
     {
         this.sp = sp;
-        HashMap<String, Double> featureMap = new HashMap<String, Double>();
+        featureMap.clear();
         
-        double[] overlaps = ngramOverlaps();
-        double[] numberOverlaps = numberOverlaps();
-        featureMap.put("UniGramOverlap", overlaps[0]);
-        featureMap.put("BiGramOverlap", overlaps[1]);
-        featureMap.put("TriGramOverlap", overlaps[2]);
-        featureMap.put("NumberOverlap1", numberOverlaps[0]);
-        featureMap.put("NumberOverlap2", numberOverlaps[1]);
-        featureMap.put("NumberOverlap3", numberOverlaps[2]);
-        featureMap.put("CapitalizedOverlap", capitalizedOverlap());
-        featureMap.put("StockOverlap", stockOverlap());
-        featureMap.put("WordNetOverlap", wordnetOverlap());
+        ngramOverlaps();
+        numberOverlaps();
+        capitalizedOverlap();
+        stockOverlap();
+        wordnetOverlap();
+        weightedWords();
         
         printMap(featureMap);
-        
         return featureMap;
     }
     
@@ -58,9 +66,14 @@ public class FeatureCollector
         }
     }
     
-    private double wordnetOverlap()
+    private void weightedWords()
+    {
+        
+    }
+    
+    private void wordnetOverlap()
     {        
-        return harmonicMean(pwn(sp.s1, sp.s2), pwn(sp.s2, sp.s1));
+        featureMap.put("WordNetOverlap", harmonicMean(pwn(sp.s1, sp.s2), pwn(sp.s2, sp.s1)) );
     }
     
     private double harmonicMean(double d1, double d2)
@@ -109,10 +122,8 @@ public class FeatureCollector
         return maxScore;
     }
     
-    private double[] ngramOverlaps()
+    private void ngramOverlaps()
     {
-        double[] overlaps = new double[3];
-        
         Set<String> s1unigrams = new HashSet<String>();
         Set<String> s1bigrams = new HashSet<String>();
         Set<String> s1trigrams = new HashSet<String>();
@@ -152,28 +163,54 @@ public class FeatureCollector
             }
         }
         
-        overlaps[0] = setOverlap(s1unigrams, s2unigrams);
-        overlaps[1] = setOverlap(s1bigrams, s2bigrams);
-        overlaps[2] = setOverlap(s1trigrams, s2trigrams);
-        
-        return overlaps;
+        featureMap.put("UniGramOverlap", setOverlap(s1unigrams, s2unigrams));
+        featureMap.put("BiGramOverlap", setOverlap(s1bigrams, s2bigrams));
+        featureMap.put("TriGramOverlap", setOverlap(s1trigrams, s2trigrams));
     }
 
-    private double[] numberOverlaps()
+    private void numberOverlaps()
     {
         Set<Double> n1 = numberTokens(sp.s1);
         Set<Double> n2 = numberTokens(sp.s2);        
         double[] results = new double[3];
         
         results[0] = Math.log(1 + n1.size() + n2.size());
-        results[1] = (n1.containsAll(n2) || n2.containsAll(n1)) ? 1.0 : 0.0;
+        results[1] = (modifiedContainsAll(n1, n2) || modifiedContainsAll(n2, n1)) ? 1.0 : 0.0;
         results[2] = 1.0;
         results[2] /= (n1.size() + n2.size());
         
         n1.retainAll(n2);
         results[2] *= n1.size() * 2;
         
-        return results;
+        featureMap.put("NumberOverlap1", results[0]);
+        featureMap.put("NumberOverlap2", results[1]);
+        featureMap.put("NumberOverlap3", results[2]);
+    }
+    
+    private boolean modifiedContainsAll(Set<Double> n1, Set<Double> n2)
+    {
+        for (Iterator<Double> it = n1.iterator(); it.hasNext(); ) {
+            Double d1 = it.next();
+            BigDecimal bg1 = new BigDecimal(d1);
+            String number1 = bg1.toString();
+            
+            for (Iterator<Double> it2 = n2.iterator(); it2.hasNext(); ) {
+                Double d2 = it2.next();
+                BigDecimal bg2 = new BigDecimal(d2);
+                String number2 = bg2.toString();
+                
+                if (d1.intValue() != d2.intValue()) {
+                    return false;
+                }
+                
+                if (!number1.startsWith(number2) && !number2.startsWith(number1) ) {
+                    return false;
+                }
+            }
+            
+        }
+        
+        return true;
     }
     
     private Set<Double> numberTokens(List<TaggedToken> sentence)
@@ -190,21 +227,25 @@ public class FeatureCollector
         
     }
     
-    private double capitalizedOverlap()
+    private void capitalizedOverlap()
     {
         Set<String> capTokens1 = findCapitalizedWords(sp.s1);
         Set<String> capTokens2 = findCapitalizedWords(sp.s2);
-        return setOverlap(capTokens1, capTokens2);
+        double presence = (!capTokens1.isEmpty() || !capTokens2.isEmpty()) ? 1.0 : 0.0;
+        
+        featureMap.put("CapitalizedOverlap", setOverlap(capTokens1, capTokens2) );
+        featureMap.put("CapitalizedPresence", presence);
     }
     
     private Set<String> findCapitalizedWords(List<TaggedToken> sentence)
     {
         Set<String> cap = new HashSet<String>();
         
-        for (TaggedToken tt : sentence) {
-            String token = tt.token;
+        for (int i = 0; i < sentence.size(); i++) {
+            String token = sentence.get(i).token;
             
-            if (token.length() > 1 && Character.isUpperCase( token.charAt(0) )) {
+            if (token.length() > 1 && Character.isUpperCase( token.charAt(0) ) &&
+                    (i == 0 || !sentence.get(i-1).token.equals("."))) {
                 cap.add(token);
             }
         }
@@ -212,12 +253,14 @@ public class FeatureCollector
         return cap;
     }
 
-    private double stockOverlap()
+    private void stockOverlap()
     {
         Set<String> stockItems1 = findStockItems(sp.s1);
         Set<String> stockItems2 = findStockItems(sp.s2);
+        double presence = (!stockItems1.isEmpty() || !stockItems2.isEmpty()) ? 1.0 : 0.0;
         
-        return setOverlap(stockItems1, stockItems2);
+        featureMap.put("StockOverlap", setOverlap(stockItems1, stockItems2));
+        featureMap.put("StockPresence", presence);
     }
    
     private Set<String> findStockItems(List<TaggedToken> sentence)
@@ -227,8 +270,9 @@ public class FeatureCollector
         for (int i = 0; i < sentence.size(); i++) {
             String token = sentence.get(i).token;
             
-            if (isUpper(token)) {
+            if (isUpper(token) && isWord(token)) {
                 if (token.startsWith(".") || (i > 0 && sentence.get(i-1).token.equals("."))) {
+                    System.out.println("\"" + token + "\" is a stock item");
                     stockItems.add(token);
                 }
             }
@@ -248,13 +292,29 @@ public class FeatureCollector
         return true;
     }
     
+    private boolean isWord(String s)
+    {
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter( s.charAt(i) )) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     private <T> double setOverlap(Set<T> set1, Set<T> set2)
     {
         double overlap = (set1.size() + set2.size());
         set1.retainAll(set2);
-        overlap /= set1.size();
-        overlap = Math.pow(overlap, -1.0);
-        overlap *= 2;
+        
+        if (!set1.isEmpty()) {
+            overlap /= set1.size();
+            overlap = Math.pow(overlap, -1.0);
+            overlap *= 2;
+        } else {
+            overlap = 0;
+        }
         
         return overlap;
     }
