6b1848e0e613cf2fec04452647390d55a7528705
Lisa Vitolo <syn.shainer@gmail.com>
2013-06-21 18:31:47 +0200

    New script for one-letter words

---
+++ scripts/oneLetterWords.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+
+def findOneLetterWords(path, oneLetterFile):
+	p = open(path, "r")
+
+	for line in p:
+		fields = line.split("\t")
+		pieces = fields[0].split("_")
+		word = pieces[0]
+		rest = ''.join(pieces[1:])
+
+		if len(word) == 1 and rest.isupper():
+			oneLetterFile.write(word.lower() + "_" + rest + "\t" + fields[1])
+
+	p.close()
+
+if __name__ == "__main__":
+	oneLetterFile = open("oneLetter", "w")
+
+	for path in sys.argv[1:]:
+		findOneLetterWords(path, oneLetterFile)
+		print ":: File " + path + " completed"
+
+	oneLetterFile.close()
