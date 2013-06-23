ef1fa962130dc0f1b5e7451269dc874aad9a3180
Lisa Vitolo <syn.shainer@gmail.com>
2013-06-23 21:05:22 +0200

    Preprocessing function added to SentencePair

--- SentencePair.java
+++ SentencePair.java
@@ -4,7 +4,9 @@
  */
 
 import java.util.List;
+import java.util.LinkedList;
 import cmu.arktweetnlp.Tagger.TaggedToken;
+import cmu.arktweetnlp.Tagger;
 
 /**
  *
@@ -15,10 +17,25 @@ public class SentencePair
     public List<TaggedToken> s1;
     public List<TaggedToken> s2;
 
+    public SentencePair()
+    {}
+    
     public SentencePair(List<TaggedToken> s1, List<TaggedToken> s2)
     {
         this.s1 = s1;
         this.s2 = s2;
+    }
+    
+    public SentencePair(List<TaggedToken> s1, String s2, Tagger tagger)
+    {
+        this.s1 = s1;
+        this.s2 = preprocess(s2, tagger);
+    }
+    
+    public SentencePair(String s1, String s2, Tagger tagger)
+    {
+        this.s1 = preprocess(s1, tagger);
+        this.s2 = preprocess(s2, tagger);
     }
     
     public String toString()
@@ -38,6 +55,11 @@ public class SentencePair
         p += "\n";
         
         return p;
+    }
+    
+    private List<TaggedToken> preprocess(String s, Tagger t)
+    {
+        return t.tokenizeAndTag(s);
     }
 } 
 
