75f29a2ecfd3d5821e79809b9cd6fc886b2aab06
Lisa Vitolo <syn.shainer@gmail.com>
2013-07-15 21:50:06 +0200

    Fixing script for min and max

--- scripts/printMinAndMax.py
+++ scripts/printMinAndMax.py
@@ -11,25 +11,26 @@ if __name__ == "__main__":
   outputFile = open(sys.argv[2], "w")
   maximum = []
   minimum = []
+  featureSize = 14
   
-  for i in range(0, 16):
+  for i in range(0, featureSize):
     maximum.append(0)
     minimum.append(25000)
     
   for line in inputFile:
     features = line.split("\t")
-    line = line[:-1]
-    
-    for i in range(0, 16):
-      if float(line[i]) > maximum[i]:
-	maximum[i] = line[i]
-      if float(line[i]) < minimum[i]:
-	minimum[i] = line[i]
+    features = features[:-1]
+        
+    for i in range(0, featureSize):
+      if float(features[i]) > maximum[i]:
+	maximum[i] = features[i]
+      if float(features[i]) < minimum[i]:
+	minimum[i] = features[i]
 	
 
-  for i in range(0, 16):
-    outputFile.write("Max: " + maximum[i] + "\n")
-    outputFile.write("Min: " + minimum[i] + "\n\n")
+  for i in range(0, featureSize):
+    outputFile.write("Max: " + str(maximum[i]) + "\n")
+    outputFile.write("Min: " + str(minimum[i]) + "\n\n")
   
   inputFile.close()
   outputFile.close()
